fc338f8bbd59909a79086814faab358e76b40a56
Nisha271 <bukkenisha@gmail.com>
2020-07-11 00:34:37 +0530

    Newely created Branch UC-5 and Modified file for Expression 4 computation

--- arithematic_ComputationAndSorting.sh
+++ arithematic_ComputationAndSorting.sh
@@ -5,4 +5,5 @@ read -p "Enter Third Number" thirdNumber
 ExpressionResult_1=$(( firstNumber + secondNumber * thirdNumber ))
 ExpressionResult_2=$(( firstNumber * secondNumber + thirdNumber ))
 ExpressionResult_3=$(( thirdNumber * firstNumber / secondNumber ))
+ExpressionResult_4=$(( firstNumber % secondNumber + thirdNumber ))
 
