58772aff3905d3451dc3d357d9602b96397664fb
Nisha271 <bukkenisha@gmail.com>
2020-07-11 00:22:22 +0530

    Newely Created Branch UC-2 and modified file with Expression first computation

--- arithematic_ComputationAndSorting.sh
+++ arithematic_ComputationAndSorting.sh
@@ -2,4 +2,5 @@
 read -p "Enter First Number" firstNumber
 read -p "Enter Second Number" secondNumber
 read -p "Enter Third Number" thirdNumber
+ExpressionResult_1=$(( firstNumber + secondNumber * thirdNumber ))
 
